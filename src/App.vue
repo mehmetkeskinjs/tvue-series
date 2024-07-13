3cacd461ff69745c6064531b4dc6c813cd33737d
Mehmet Keskin <keskinm90@gmail.com>
2024-07-13 17:33:33 +0300

    created dummy data for movies

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
     <Header :isVisible="isInputVisible" @toggleInput="toggleInput" />
     <GlobalSearch :isVisible="isInputVisible" />
     <HeroSection />
+    <div class="p-8">
+        <h1>Movies</h1>
+        <ul v-if="movies.length" class="flex flex-wrap gap-4">
+          <li v-for="movie in movies" :key="movie.id" class="border max-w-[150px]">
+            <img :src="movie.image" alt="Movie Image" />
+            <h2>{{ movie.title }}</h2>
+            <p>Rating: {{ movie.rating }}</p>
+            <p>Release Date: {{ movie.date }}</p>
+          </li>
+        </ul>
+        <p v-else>Loading movies...</p>
+      </div>
 </template>
 
 <script setup>
@@ -9,25 +21,34 @@
     import GlobalSearch from './components/GlobalSearch.vue';
     import HeroSection from './components/HeroSection.vue';
     import { ref, onMounted } from 'vue';
+    import { movies as moviesData } from './assets/movies.js'
 
     const movie = ref(null);
+    const movies = ref([]);
     const isInputVisible = ref(false);
 
-    const fetchMovies = async () => {
-        try {
-            const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
-            const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${ API_KEY }`);
-            const data = await response.json();
+    // const fetchMovies = async () => {
+    //     try {
+    //         const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
+    //         const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${ API_KEY }`);
+    //         const data = await response.json();
             
-            movie.value = data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    //         movie.value = data;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
 
     const toggleInput = () => {
         isInputVisible.value = !isInputVisible.value;
     };
 
-    onMounted(fetchMovies);
+    const setMovies = () => {
+        setTimeout(() => {
+            movies.value = moviesData;
+        }, 1000);
+        console.log(movies)
+    }
+
+    onMounted(setMovies);
 </script>
---
+++ src/assets/movies.js
@@ -0,0 +1,72 @@
+export const movies = [
+    {
+        "id": 1,
+        "image": "https://via.placeholder.com/150",
+        "title": "Inception",
+        "rating": 8.8,
+        "date": "2010-07-16"
+      },
+      {
+        "id": 2,
+        "image": "https://via.placeholder.com/150",
+        "title": "The Dark Knight",
+        "rating": 9.0,
+        "date": "2008-07-18"
+      },
+      {
+        "id": 3,
+        "image": "https://via.placeholder.com/150",
+        "title": "Interstellar",
+        "rating": 8.6,
+        "date": "2014-11-07"
+      },
+      {
+        "id": 4,
+        "image": "https://via.placeholder.com/150",
+        "title": "The Matrix",
+        "rating": 8.7,
+        "date": "1999-03-31"
+      },
+      {
+        "id": 5,
+        "image": "https://via.placeholder.com/150",
+        "title": "Fight Club",
+        "rating": 8.8,
+        "date": "1999-10-15"
+      },
+      {
+        "id": 6,
+        "image": "https://via.placeholder.com/150",
+        "title": "The Shawshank Redemption",
+        "rating": 9.3,
+        "date": "1994-09-23"
+      },
+      {
+        "id": 7,
+        "image": "https://via.placeholder.com/150",
+        "title": "Pulp Fiction",
+        "rating": 8.9,
+        "date": "1994-10-14"
+      },
+      {
+        "id": 8,
+        "image": "https://via.placeholder.com/150",
+        "title": "Forrest Gump",
+        "rating": 8.8,
+        "date": "1994-07-06"
+      },
+      {
+        "id": 9,
+        "image": "https://via.placeholder.com/150",
+        "title": "The Lord of the Rings: The Fellowship of the Ring",
+        "rating": 8.8,
+        "date": "2001-12-19"
+      },
+      {
+        "id": 10,
+        "image": "https://via.placeholder.com/150",
+        "title": "The Godfather",
+        "rating": 9.2,
+        "date": "1972-03-24"
+      }
+]
