4e24f269a4b9ef8fe78acc2763315484d5937d6e
= <=>
2024-08-02 00:25:00 +0300

    componentize movies slider, tv shows next

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,7 @@
     <HeroSection />
 
     <!-- TODO: Componentize this -->
-    <div class="container px-12 py-8">
-        <h1 class="text-3xl font-medium">Movies</h1>
-
-        <div v-if="isMoviesLoading" class="flex gap-2 pb-4 pt-6">
-            <MovieCardSkeleton v-for="n in 6" :key="n" />
-        </div>
-        <ul
-            v-else
-            class="flex w-full min-w-full flex-row gap-2 overflow-auto pb-4 pt-6"
-        >
-            <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
-        </ul>
-    </div>
+    <MoviesSlider title="Movies" :isMoviesLoading="isMoviesLoading" :movies="movies"/>
 
     <div class="bg-zinc-900 py-8">
         <div class="container px-12">
@@ -44,6 +32,7 @@ import Header from './components/Header/Header.vue';
 import GlobalSearch from './components/GlobalSearch.vue';
 import HeroSection from './components/HeroSection/HeroSection.vue';
 import MovieCard from './components/MovieCard.vue';
+import MoviesSlider from './components/MoviesSlider/MoviesSlider.vue';
 import { ref, onMounted } from 'vue';
 import MovieCardSkeleton from './components/MovieCardSkeleton.vue';
 
---
+++ src/components/MoviesSlider/MoviesSlider.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container px-12 py-8">
+        <h1 class="text-3xl font-medium">{{ title }}</h1>
+
+        <div v-if="isMoviesLoading" class="flex gap-2 pb-4 pt-6">
+            <MovieCardSkeleton v-for="n in 6" :key="n" />
+        </div>
+        <ul
+            v-else
+            class="flex w-full min-w-full flex-row gap-2 overflow-auto pb-4 pt-6"
+        >
+            <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import MovieCardSkeleton from "../MovieCardSkeleton.vue"
+import MovieCard from "../MovieCard.vue";
+
+defineProps({
+    title: {
+        type: String,
+        required: true
+    }, 
+    isMoviesLoading: {
+        type: Boolean,
+        required: true
+    },
+    movies: {
+        type: Array,
+        required: true
+    }
+});
+</script>
